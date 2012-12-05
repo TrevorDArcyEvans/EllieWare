ca536742300986822dc8ce0e471f706e8c9b3fca
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2012-12-05 22:01:40 +0000

    - skeleton of DateTimeLog

---
+++ Code/EllieWare.DateTime/DateTimeLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using EllieWare.Common;
+using EllieWare.Interfaces;
+
+namespace EllieWare.DateTime
+{
+  public partial class DateTimeLog : MutableRunnableBase
+  {
+    public DateTimeLog()
+    {
+      InitializeComponent();
+    }
+
+    public DateTimeLog(object root, ICallback callback, IParameterManager mgr) :
+      base(root, callback, mgr)
+    {
+      InitializeComponent();
+    }
+}
+}
---
+++ Code/EllieWare.DateTime/DateTimeLogFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EllieWare.Interfaces;
+
+namespace EllieWare.DateTime
+{
+  public class DateTimeLogFactory : IFactory
+  {
+    #region Implementation of IFactory
+
+    public string Title
+    {
+      get { return "Log current date and time"; }
+    }
+
+    public string Description
+    {
+      get { return "Log current date and time"; }
+    }
+
+    public string Keywords
+    {
+      get { return "log, date, time, "; }
+    }
+
+    public IEnumerable<string> Categories
+    {
+      get { return new[] { "Logging", "Debugging" }; }
+    }
+
+    public Type CreatedType
+    {
+      get { return typeof(DateTimeLog); }
+    }
+
+    public IRunnable Create(object root, ICallback callback, IParameterManager mgr)
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+  }
+}
