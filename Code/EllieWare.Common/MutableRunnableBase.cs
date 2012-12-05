e0ca5732a004d5b5b21c1ee5a0dcf05dcd527c43
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2012-12-05 22:02:22 +0000

    - added IMutableRunnable

---
+++ Code/EllieWare.Common/MutableRunnableBase.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Schema;
+using EllieWare.Interfaces;
+
+namespace EllieWare.Common
+{
+  public partial class MutableRunnableBase : UserControl, IMutableRunnable
+  {
+    protected readonly object mRoot;
+    protected readonly ICallback mCallback;
+    protected readonly IParameterManager mParamMgr;
+
+    public MutableRunnableBase()
+    {
+      InitializeComponent();
+    }
+
+    public MutableRunnableBase(object root, ICallback callback, IParameterManager mgr) :
+      this()
+    {
+      mRoot = root;
+      mCallback = callback;
+      mParamMgr = mgr;
+    }
+
+    #region Implementation of IXmlSerializable
+
+    public XmlSchema GetSchema()
+    {
+      return null;
+    }
+
+    public virtual void ReadXml(XmlReader reader)
+    {
+      throw new NotImplementedException();
+    }
+
+    public virtual void WriteXml(XmlWriter writer)
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+
+    #region Implementation of IRunnable
+
+    public virtual string Description
+    {
+      get { throw new NotImplementedException(); }
+    }
+
+    public virtual Control ConfigurationUserInterface
+    {
+      get { throw new NotImplementedException(); }
+    }
+
+    public virtual bool Run()
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+
+    protected void FireConfigurationChanged()
+    {
+      if (ConfigurationChanged != null)
+      {
+        ConfigurationChanged(this, new EventArgs());
+      }
+    }
+
+    #region Implementation of IMutableRunnable
+
+    public event EventHandler ConfigurationChanged;
+
+    #endregion
+  }
+}
