6b58602c0fb3fecdde9ba945ce47d8fcce3b7a89
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2012-12-07 17:48:04 +0000

    - implemented object de/serialisation tests for parameters - code cleanup

---
+++ Code/EllieWare.Common.Tests/ParameterManager.Persistence.Tests.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using EllieWare.Interfaces;
+using NUnit.Framework;
+
+namespace EllieWare.Common.Tests
+{
+  [TestFixture]
+  public class ParameterManagerPersistenceTests
+  {
+    private IParameterManager mParamMgr;
+
+    #region SetUp/TearDown
+
+    [TestFixtureSetUp]
+    public void FixtureSetUp()
+    {
+    }
+
+    [TestFixtureTearDown]
+    public void FixtureTearDown()
+    {
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+      mParamMgr = new ParameterManager();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+    }
+
+    #endregion
+
+    private string GetXml()
+    {
+      string xmlTxt;
+      using (var sw = new StringWriter())
+      {
+        using (var writer = XmlWriter.Create(sw))
+        {
+          writer.WriteStartElement("Specification");
+          writer.WriteStartElement("ParameterManager");
+
+          mParamMgr.WriteXml(writer);
+
+          writer.WriteEndElement();
+          writer.WriteEndElement();
+        }
+
+        xmlTxt = sw.ToString();
+      }
+      return xmlTxt;
+    }
+
+    private IParameterManager CreateParameterManager(string xmlTxt)
+    {
+      var reader = XmlReader.Create(new StringReader(xmlTxt));
+      var newParamMgr = new ParameterManager();
+      newParamMgr.ReadXml(reader);
+
+      return newParamMgr;
+    }
+
+    [Test]
+    public void ParameterManager_Persistence_String()
+    {
+      // string
+      mParamMgr.Add("Key1", "Param1");
+      mParamMgr.Add("Key2", "Param2");
+
+      var newParamMgr = CreateParameterManager(GetXml());
+
+      Assert.True(newParamMgr.DisplayNames.Count() == 2);
+
+      Assert.True(newParamMgr.DisplayNames.Contains("Key1"));
+      Assert.True(newParamMgr.DisplayNames.Contains("Key2"));
+
+      Assert.AreEqual(newParamMgr.Get("Key1").GetType(), typeof(string));
+      Assert.AreEqual(newParamMgr.Get("Key2").GetType(), typeof(string));
+
+      Assert.AreEqual(newParamMgr.Get("Key1"), "Param1");
+      Assert.AreEqual(newParamMgr.Get("Key2"), "Param2");
+    }
+
+    [Test]
+    public void ParameterManager_Persistence_Boolean()
+    {
+      // boolean
+      mParamMgr.Add("Key1", true);
+      mParamMgr.Add("Key2", false);
+
+      var newParamMgr = CreateParameterManager(GetXml());
+
+      Assert.True(newParamMgr.DisplayNames.Count() == 2);
+
+      Assert.True(newParamMgr.DisplayNames.Contains("Key1"));
+      Assert.True(newParamMgr.DisplayNames.Contains("Key2"));
+
+      Assert.AreEqual(newParamMgr.Get("Key1").GetType(), typeof(bool));
+      Assert.AreEqual(newParamMgr.Get("Key2").GetType(), typeof(bool));
+
+      Assert.AreEqual(newParamMgr.Get("Key1"), true);
+      Assert.AreEqual(newParamMgr.Get("Key2"), false);
+    }
+
+    [Test]
+    public void ParameterManager_Persistence_Number()
+    {
+      // number
+      mParamMgr.Add("Key1", 6);
+      mParamMgr.Add("Key2", 7.21f);
+
+      var newParamMgr = CreateParameterManager(GetXml());
+
+      Assert.True(newParamMgr.DisplayNames.Count() == 2);
+
+      Assert.True(newParamMgr.DisplayNames.Contains("Key1"));
+      Assert.True(newParamMgr.DisplayNames.Contains("Key2"));
+
+      Assert.AreEqual(newParamMgr.Get("Key1").GetType(), typeof(int));
+      Assert.AreEqual(newParamMgr.Get("Key2").GetType(), typeof(float));
+
+      Assert.AreEqual(newParamMgr.Get("Key1"), 6);
+      Assert.AreEqual(newParamMgr.Get("Key2"), 7.21f);
+    }
+
+    [Test]
+    public void ParameterManager_Persistence_DateTime()
+    {
+      // date-time
+      mParamMgr.Add("Key1", new DateTime(2006, 02, 20));
+      mParamMgr.Add("Key2", new DateTime(1963, 10, 19));
+
+      var newParamMgr = CreateParameterManager(GetXml());
+
+      Assert.True(newParamMgr.DisplayNames.Count() == 2);
+
+      Assert.True(newParamMgr.DisplayNames.Contains("Key1"));
+      Assert.True(newParamMgr.DisplayNames.Contains("Key2"));
+
+      Assert.AreEqual(newParamMgr.Get("Key1").GetType(), typeof(DateTime));
+      Assert.AreEqual(newParamMgr.Get("Key2").GetType(), typeof(DateTime));
+
+      Assert.AreEqual(newParamMgr.Get("Key1"), new DateTime(2006, 02, 20));
+      Assert.AreEqual(newParamMgr.Get("Key2"), new DateTime(1963, 10, 19));
+    }
+
+    [Test]
+    public void ParameterManager_Persistence_StringArray()
+    {
+      // string array
+      mParamMgr.Add("Key1", new[] { "Key1.1", "Key1.2", "Key1.3" });
+      mParamMgr.Add("Key2", new[] { "Key2.1", "Key2.2", "Key2.3" });
+
+      var newParamMgr = CreateParameterManager(GetXml());
+
+      Assert.True(newParamMgr.DisplayNames.Count() == 2);
+
+      Assert.True(newParamMgr.DisplayNames.Contains("Key1"));
+      Assert.True(newParamMgr.DisplayNames.Contains("Key2"));
+
+      Assert.AreEqual(newParamMgr.Get("Key1").GetType(), typeof(string[]));
+      Assert.AreEqual(newParamMgr.Get("Key2").GetType(), typeof(string[]));
+
+      var array1 = (string[])newParamMgr.Get("Key1");
+      Assert.True(array1.Count() == 3);
+      Assert.AreEqual(array1[0], "Key1.1");
+      Assert.AreEqual(array1[1], "Key1.2");
+      Assert.AreEqual(array1[2], "Key1.3");
+
+      var array2 = (string[])newParamMgr.Get("Key2");
+      Assert.True(array2.Count() == 3);
+      Assert.AreEqual(array2[0], "Key2.1");
+      Assert.AreEqual(array2[1], "Key2.2");
+      Assert.AreEqual(array2[2], "Key2.3");
+    }
+  }
+}
--- Code/EllieWare.Common.Tests/ParameterManagerTests.cs
+++ Code/EllieWare.Common.Tests/ParameterManagerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using EllieWare.Interfaces;
 using NUnit.Framework;
 
@@ -7,11 +8,9 @@ namespace EllieWare.Common.Tests
   [TestFixture]
   public class ParameterManagerTests
   {
-    private const string LocalisedDisplayName = "Localised display name";
-
-    private Guid mKey1 = new Guid("A99B7E87-2589-4AE7-B500-31BBD43577F8");
-    private Guid mKey2 = new Guid("A99B7E87-2589-4AE7-B500-31BBD43577F8");
-    private IParameter mParam = new StubParameter { DisplayName = LocalisedDisplayName, Value = "some string" };
+    private const string mKey1 = "A99B7E87-2589-4AE7-B500-31BBD43577F8";
+    private const string mKey2 = "A99B7E87-2589-4AE7-B500-31BBD43577F8";
+    private object mParam = "some string";
     private IParameterManager mParamMgr;
 
     #region SetUp/TearDown
@@ -40,27 +39,35 @@ namespace EllieWare.Common.Tests
     #endregion
 
     [Test]
-    public void ParameterManager_DoesNotContainNewGuid()
+    public void ParameterManager_DoesNotContainRandomKey()
     {
-      Assert.False(mParamMgr.Contains(Guid.NewGuid()));
+      Assert.False(mParamMgr.Contains(Guid.NewGuid().ToString()));
     }
 
     [Test]
-    public void ParameterManager_Add_ContainsGuid()
+    public void ParameterManager_Add_ContainsKey()
     {
       mParamMgr.Add(mKey1, mParam);
       Assert.True(mParamMgr.Contains(mKey1));
     }
 
     [Test]
-    public void ParameterManager_Add_ReturnsParameter()
+    [ExpectedException(typeof(ArgumentException))]
+    public void ParameterManager_AddSameKey_ThrowsException()
     {
       mParamMgr.Add(mKey1, mParam);
-      Assert.AreSame(mParam, mParamMgr.Get(mKey1));
+      mParamMgr.Add(mKey1, mParam);
     }
 
     [Test]
-    public void ParameterManager_Add_ContainsSameGuid()
+    public void ParameterManager_Add_ReturnsSameObject()
+    {
+      mParamMgr.Add(mKey1, mParam);
+      Assert.AreEqual(mParam, mParamMgr.Get(mKey1));
+    }
+
+    [Test]
+    public void ParameterManager_Add_ContainsSameKey()
     {
       mParamMgr.Add(mKey1, mParam);
       Assert.True(mParamMgr.Contains(mKey1));
@@ -68,24 +75,38 @@ namespace EllieWare.Common.Tests
     }
 
     [Test]
-    public void ParameterManager_GetSameGuid_ReturnsSameParameter()
+    public void ParameterManager_GetSameKey_ReturnsSameParameter()
     {
       mParamMgr.Add(mKey1, mParam);
       Assert.AreSame(mParamMgr.Get(mKey1), mParamMgr.Get(mKey2));
     }
 
     [Test]
-    public void ParameterManager_GetSameGuid_ReturnsSameValue()
+    public void ParameterManager_GetSameKey_ReturnsSameValue()
     {
       mParamMgr.Add(mKey1, mParam);
-      Assert.AreSame(mParamMgr.Get(mKey1).Value, mParamMgr.Get(mKey2).Value);
+      Assert.AreSame(mParamMgr.Get(mKey1), mParamMgr.Get(mKey2));
     }
 
     [Test]
-    public void ParameterManager_GetSameGuid_ReturnsSameDisplayName()
+    public void ParameterManager_GetSameKey_ReturnsSameDisplayName()
     {
       mParamMgr.Add(mKey1, mParam);
-      Assert.AreSame(mParamMgr.Get(mKey1).DisplayName, mParamMgr.Get(mKey2).DisplayName);
+      Assert.AreSame(mParamMgr.Get(mKey1), mParamMgr.Get(mKey2));
+    }
+
+    [Test]
+    public void ParameterManager_GetDisplayNames_ContainsExpectedDisplayName()
+    {
+      mParamMgr.Add(mKey1, mParam);
+      Assert.True(mParamMgr.DisplayNames.Contains(mKey1));
+    }
+
+    [Test]
+    public void ParameterManager_GetDisplayNames_ContainsExpectedCount()
+    {
+      mParamMgr.Add(mKey1, mParam);
+      Assert.True(mParamMgr.DisplayNames.Count() == 1);
     }
   }
 }
