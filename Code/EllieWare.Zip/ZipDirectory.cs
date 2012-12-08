5613ba4cfd309f15f7b1b83d6ce3a9bfbe3227b1
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2012-12-08 19:03:50 +0000

    - added ZipFile/Directory

---
+++ Code/EllieWare.Zip/ZipDirectory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using EllieWare.Common;
+using EllieWare.Interfaces;
+
+namespace EllieWare.Zip
+{
+  public class ZipDirectory : SingleItemIOBase
+  {
+    public ZipDirectory(object root, ICallback callback, IParameterManager mgr) :
+      base(root, callback, mgr, false)
+    {
+    }
+
+    public override string Description
+    {
+      get
+      {
+        var descrip = string.Format("Compress {0} and all subdirectories to a zip file", mSourceFilePath.Text);
+
+        return descrip;
+      }
+    }
+
+    public override bool Run()
+    {
+      try
+      {
+        //File.Delete(mSourceFilePath.Text);
+      }
+      catch (Exception ex)
+      {
+        mCallback.Log(LogLevel.Critical, ex.Message);
+
+        return false;
+      }
+
+      return true;
+    }
+  }
+}
---
+++ Code/EllieWare.Zip/ZipFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using EllieWare.Common;
+using EllieWare.Interfaces;
+
+namespace EllieWare.Zip
+{
+  public class ZipFile : SingleItemIOBase
+  {
+    public ZipFile(object root, ICallback callback, IParameterManager mgr) :
+      base(root, callback, mgr, true)
+    {
+    }
+
+    public override string Description
+    {
+      get
+      {
+        var descrip = string.Format("Compress {0} to a zip file", mSourceFilePath.Text);
+
+        return descrip;
+      }
+    }
+
+    public override bool Run()
+    {
+      try
+      {
+        //File.Delete(mSourceFilePath.Text);
+      }
+      catch (Exception ex)
+      {
+        mCallback.Log(LogLevel.Critical, ex.Message);
+
+        return false;
+      }
+
+      return true;
+    }
+  }
+}
