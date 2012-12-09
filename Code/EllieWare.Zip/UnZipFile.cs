a1d206dabc59c55b647cc5b8608675393ba460ca
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2012-12-09 14:56:18 +0000

    - added UnZip file to directory

---
+++ Code/EllieWare.Zip/UnZipFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using EllieWare.Common;
+using EllieWare.Interfaces;
+using Ionic.Zip;
+
+namespace EllieWare.Zip
+{
+  public class UnZipFile : DualItemIOBase
+  {
+    public UnZipFile(object root, ICallback callback, IParameterManager mgr) :
+      base(root, callback, mgr, true)
+    {
+    }
+
+    public override string Description
+    {
+      get
+      {
+        var descrip = string.Format("Uncompress {0} to {1}", mSourceFilePath.Text, mDestinationFilePath.Text);
+
+        return descrip;
+      }
+    }
+
+    public override bool Run()
+    {
+      try
+      {
+        using (var zip = Ionic.Zip.ZipFile.Read(mSourceFilePath.Text))
+        {
+          zip.ExtractExistingFile = ExtractExistingFileAction.OverwriteSilently;
+          zip.ExtractAll(mDestinationFilePath.Text);
+        }
+      }
+      catch (Exception ex)
+      {
+        mCallback.Log(LogLevel.Critical, ex.Message);
+
+        return false;
+      }
+
+      return true;
+    }
+  }
+}
---
+++ Code/EllieWare.Zip/UnZipFileFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using EllieWare.Interfaces;
+
+namespace EllieWare.Zip
+{
+  public class UnZipFileFactory : ZipFactoryBase, IFactory
+  {
+    public string Title
+    {
+      get
+      {
+        return "Uncompress a zip archive into a directory";
+      }
+    }
+
+    public string Description
+    {
+      get
+      {
+        return "Uncompress a zip archive into a directory";
+      }
+    }
+
+    public Type CreatedType
+    {
+      get
+      {
+        return typeof(UnZipFile);
+      }
+    }
+
+    public IRunnable Create(object root, ICallback callback, IParameterManager mgr)
+    {
+      return new UnZipFile(root, callback, mgr);
+    }
+  }
+}
