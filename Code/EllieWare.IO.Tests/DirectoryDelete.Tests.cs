1a44f51f2e5524fcf309ca518737df6b12c306e9
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2012-12-18 21:01:54 +0000

    - added Moq - added tests for: -- DirectoryDelete -- DirectoryCreate -- FileDelete

---
+++ Code/EllieWare.IO.Tests/DirectoryDelete.Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace EllieWare.IO.Tests
+{
+  [TestFixture]
+  public class DirectoryDeleteTests
+  {
+    #region SetUp/TearDown
+
+    [TestFixtureSetUp]
+    public void FixtureSetUp()
+    {
+    }
+
+    [TestFixtureTearDown]
+    public void FixtureTearDown()
+    {
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+    }
+
+    #endregion
+
+    [Test]
+    [ExpectedException(typeof(DirectoryNotFoundException))]
+    public void DirectoryDelete_DoesNotExist_ThrowsException()
+    {
+      var mockReader = Helper.GetMockReader(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), 1);
+      var delDir = new DirectoryDelete();
+
+      delDir.ReadXml(mockReader.Object);
+
+      var bRet = delDir.Run();
+    }
+
+    [Test]
+    public void DirectoryDelete_Exists_Succeeds()
+    {
+      var cwd = Environment.CurrentDirectory;
+      var tempDirName = Guid.NewGuid().ToString();
+      var tempDirPath = Path.Combine(cwd, tempDirName);
+      var mockReader = Helper.GetMockReader(tempDirPath, Guid.NewGuid().ToString(), 1);
+      var delDir = new DirectoryDelete();
+
+      delDir.ReadXml(mockReader.Object);
+      Directory.CreateDirectory(tempDirPath);
+
+      Assert.True(delDir.Run());
+      Assert.True(!Directory.Exists(tempDirPath));
+    }
+  }
+}
---
+++ Code/EllieWare.IO.Tests/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Xml;
+using Moq;
+
+namespace EllieWare.IO.Tests
+{
+  public static class Helper
+  {
+    public static Mock<XmlReader> GetMockReader(string source, string destination, int exists)
+    {
+      var mockReader = new Mock<XmlReader>();
+      mockReader.Setup(x => x.GetAttribute("Source")).Returns(source);
+      mockReader.Setup(x => x.GetAttribute("Destination")).Returns(destination);
+      mockReader.Setup(x => x.GetAttribute("Exists")).Returns(exists.ToString(CultureInfo.InvariantCulture));
+
+      return mockReader;
+    }
+  }
+}
