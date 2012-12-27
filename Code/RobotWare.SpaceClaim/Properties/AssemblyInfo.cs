08230ba305375cd6b9a645a931358f29d0a5d6f7
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2012-12-27 22:30:07 +0000

    - initial version

---
+++ Code/RobotWare.SpaceClaim/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿//
+//  Copyright (C) 2012 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RobotWare.SpaceClaim")]
+[assembly: AssemblyProduct("RobotWare.SpaceClaim")]
