9c28281bb39e934639c1decedf71ec439acdf80e
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-04 12:50:06 +0000

    - refactored - added various export file filters

--- Code/EllieWare.SpaceClaim/ImportFromForeign.cs
+++ Code/EllieWare.SpaceClaim/ImportFromForeign.cs
@@ -16,35 +16,6 @@ namespace EllieWare.SpaceClaim
 {
   public partial class ImportFromForeign : SingleItemIOBase
   {
-    private readonly Dictionary<string, PartExportFormat> BrepFormats = new Dictionary<string, PartExportFormat>
-                                              {
-                                                {".CATPart".ToLowerInvariant(), PartExportFormat.CatiaV5Part},
-                                                {".CATProduct".ToLowerInvariant(), PartExportFormat.CatiaV5Assembly},
-                                                {".cgr", PartExportFormat.CatiaV5Graphics},
-                                                {".igs", PartExportFormat.Iges},
-                                                {".iges", PartExportFormat.Iges},
-                                                {".stp", PartExportFormat.Step},
-                                                {".step", PartExportFormat.Step},
-                                                {".vda", PartExportFormat.Vda},
-                                                {".jt", PartExportFormat.JtOpen},
-                                                {".x_t", PartExportFormat.ParasolidText},
-                                                {".x_b", PartExportFormat.ParasolidBinary},
-                                                {".sat", PartExportFormat.AcisText},
-                                                {".sab", PartExportFormat.AcisBinary},
-                                                {".3dm", PartExportFormat.Rhino},
-                                                {".pdf", PartExportFormat.Pdf},
-                                                {".skp", PartExportFormat.SketchUp}
-                                              };
-
-    private readonly Dictionary<string, PartWindowExportFormat> TessellatedFormats = new Dictionary<string, PartWindowExportFormat>
-                                              {
-                                                {".bip", PartWindowExportFormat.Bip},
-                                                {".obj", PartWindowExportFormat.Obj},
-                                                {".stl", PartWindowExportFormat.Stl},
-                                                {".wrl", PartWindowExportFormat.Vrml},
-                                                {".vrml", PartWindowExportFormat.Vrml},
-                                                {".xaml", PartWindowExportFormat.Xaml}
-                                              };
     public ImportFromForeign()
     {
     }
@@ -67,17 +38,17 @@ namespace EllieWare.SpaceClaim
     private PartExportFormat Format(string fileName)
     {
       var extn = Path.GetExtension(fileName);
-      if (extn == null || !BrepFormats.ContainsKey(extn.ToLowerInvariant()))
+      if (extn == null || !Utils.BrepFormats.ContainsKey(extn.ToLowerInvariant()))
       {
         return PartExportFormat.AcisBinary;
       }
 
-      return BrepFormats[extn.ToLowerInvariant()];
+      return Utils.BrepFormats[extn.ToLowerInvariant()];
     }
 
     private string Extension(PartExportFormat fmt)
     {
-      return BrepFormats.First(x => x.Value == fmt).Key;
+      return Utils.BrepFormats.First(x => x.Value == fmt).Key;
     }
 
     public override bool Run()
---
+++ Code/EllieWare.SpaceClaim/Utils.cs
@@ -0,0 +1,80 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+
+using System.Collections.Generic;
+using SpaceClaim.Api.V10;
+
+namespace EllieWare.SpaceClaim
+{
+  public class Utils
+  {
+    public static Dictionary<string, PartExportFormat> BrepFormats = new Dictionary<string, PartExportFormat>
+                                              {
+                                                {".CATPart".ToLowerInvariant(), PartExportFormat.CatiaV5Part},
+                                                {".CATProduct".ToLowerInvariant(), PartExportFormat.CatiaV5Assembly},
+                                                {".cgr", PartExportFormat.CatiaV5Graphics},
+                                                {".igs", PartExportFormat.Iges},
+                                                {".iges", PartExportFormat.Iges},
+                                                {".stp", PartExportFormat.Step},
+                                                {".step", PartExportFormat.Step},
+                                                {".vda", PartExportFormat.Vda},
+                                                {".jt", PartExportFormat.JtOpen},
+                                                {".x_t", PartExportFormat.ParasolidText},
+                                                {".x_b", PartExportFormat.ParasolidBinary},
+                                                {".sat", PartExportFormat.AcisText},
+                                                {".sab", PartExportFormat.AcisBinary},
+                                                {".3dm", PartExportFormat.Rhino},
+                                                {".pdf", PartExportFormat.Pdf},
+                                                {".skp", PartExportFormat.SketchUp}
+                                              };
+
+    public static Dictionary<string, PartWindowExportFormat> TessellatedFormats = new Dictionary<string, PartWindowExportFormat>
+                                              {
+                                                {".bip", PartWindowExportFormat.Bip},
+                                                {".obj", PartWindowExportFormat.Obj},
+                                                {".stl", PartWindowExportFormat.Stl},
+                                                {".wrl", PartWindowExportFormat.Vrml},
+                                                {".vrml", PartWindowExportFormat.Vrml},
+                                                {".xaml", PartWindowExportFormat.Xaml}
+                                              };
+
+    private const string AllFilesFilter = "All files (*.*)|*.*";
+
+    private const string InternalPartExportFilter =
+                          "Catia V5 part files (*.CATPart)|*.CATPart|" +
+                          "Catia V5 assembly files (*.CATProduct)|*.CATProduct|" +
+                          "Catia V5 graphics files (*.cgr)|*.cgr|" +
+                          "IGES files (*.igs)|*.igs|" +
+                          "IGES files (*.iges)|*.iges|" +
+                          "STEP files (*.stp)|*.stp|" +
+                          "STEP files (*.step)|*.step|" +
+                          "VDA files (*.vda)|*.vda|" +
+                          "JT Open files (*.)|*.jt|" +
+                          "Parasolid text files (*.x_t)|*.x_t|" +
+                          "Parasolid binary files (*.x_b)|*.x_b|" +
+                          "ACIS text files (*.sat)|*.sat|" +
+                          "ACIS binary files (*.sab)|*.sab|" +
+                          "Rhino files (*.3dm)|*.3dm|" +
+                          "3D PDF files (*.pdf)|*.pdf|" +
+                          "SketchUp files (*.skp)|*.skp|";
+
+    public const string PartExportFilter = InternalPartExportFilter + AllFilesFilter;
+
+    private const string InternalPartWindowExportFilter =
+                          "VRML files (*.wrl)|*.wrl|" +
+                          "VRML files (*.vrml)|*.vrml|" +
+                          "XAML files (*.xaml)|*.xaml|" +
+                          "STL files (*.stl)|*.stl|" +
+                          "Wavefront image files (*.obj)|*.obj|" +
+                          "Luxion KeyShot scene files (*.bip)|*.bip|";
+
+    public const string PartWindowExportFilter = InternalPartWindowExportFilter + AllFilesFilter;
+
+    public const string AllPartExportFilter = InternalPartExportFilter + InternalPartWindowExportFilter + AllFilesFilter;
+  }
+}
