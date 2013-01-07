bf0ebe4b90921c9b62abec78f1f49a36964c9933
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-07 11:34:58 +0000

    - refactored

---
+++ Code/EllieWare.Interfaces/IDirectoryBatchParameter.cs
@@ -0,0 +1,18 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+
+using EllieWare.Interfaces;
+
+namespace EllieWare.Common
+{
+  public interface IDirectoryBatchParameter : IParameter
+  {
+    string FileMask { get; set; }
+    string Directory { get; set; }
+  }
+}
