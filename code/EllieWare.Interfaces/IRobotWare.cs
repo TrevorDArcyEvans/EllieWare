c6a65e45679c6439744621bd9ecd6e4990bd4ef3
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-07 09:36:15 +0000

    - moved

---
+++ code/EllieWare.Interfaces/IRobotWare.cs
@@ -0,0 +1,16 @@
+﻿//
+//  Copyright (C) 2012 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+namespace EllieWare.Common
+{
+  public interface IRobotWare
+  {
+    bool IsLicensed { get; }
+    string UserSpecificationFolder { get; }
+    string ApplicationName { get; }
+  }
+}
