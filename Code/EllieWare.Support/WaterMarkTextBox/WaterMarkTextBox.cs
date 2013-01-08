4d9216b1aa97c00d3c590c19662743d6d4659672
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-08 11:59:51 +0000

    - code cleanup

--- Code/EllieWare.Support/WaterMarkTextBox/WaterMarkTextBox.cs
+++ Code/EllieWare.Support/WaterMarkTextBox/WaterMarkTextBox.cs
@@ -2,11 +2,8 @@
 //    http://www.codeproject.com/Articles/27849/WaterMark-TextBox-For-Desktop-Applications-Using-C
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
 using System.Drawing;
+using System.Windows.Forms;
 
 namespace wmgCMS
 {
@@ -16,19 +13,35 @@ namespace wmgCMS
     private Boolean waterMarkTextEnabled = false;
 
     #region Attributes
+
     private Color _waterMarkColor = Color.Gray;
     public Color WaterMarkColor
     {
-      get { return _waterMarkColor; }
-      set { _waterMarkColor = value; Invalidate();/*thanks to Bernhard Elbl for Invalidate()*/ }
+      get
+      {
+        return _waterMarkColor;
+      }
+      set
+      {
+        _waterMarkColor = value; 
+        Invalidate();/*thanks to Bernhard Elbl for Invalidate()*/
+      }
     }
 
     private string _waterMarkText = "Water Mark";
     public string WaterMarkText
     {
-      get { return _waterMarkText; }
-      set { _waterMarkText = value; Invalidate(); }
+      get
+      {
+        return _waterMarkText;
+      }
+      set
+      {
+        _waterMarkText = value; 
+        Invalidate();
+      }
     }
+
     #endregion
 
     //Default constructor
@@ -41,18 +54,22 @@ namespace wmgCMS
     protected override void OnCreateControl()
     {
       base.OnCreateControl();
-      WaterMark_Toggel(null, null);
+
+      WaterMark_Toggle(null, null);
     }
 
     //Override OnPaint
     protected override void OnPaint(PaintEventArgs args)
     {
       // Use the same font that was defined in base class
-      System.Drawing.Font drawFont = new System.Drawing.Font(Font.FontFamily, Font.Size, Font.Style, Font.Unit);
+      Font drawFont = new Font(Font.FontFamily, Font.Size, Font.Style, Font.Unit);
+
       //Create new brush with gray color or 
       SolidBrush drawBrush = new SolidBrush(WaterMarkColor);//use Water mark color
+
       //Draw Text or WaterMark
       args.Graphics.DrawString((waterMarkTextEnabled ? WaterMarkText : Text), drawFont, drawBrush, new PointF(0.0F, 0.0F));
+
       base.OnPaint(args);
     }
 
@@ -60,53 +77,65 @@ namespace wmgCMS
     {
       if (join)
       {
-        this.TextChanged += new System.EventHandler(this.WaterMark_Toggel);
-        this.LostFocus += new System.EventHandler(this.WaterMark_Toggel);
-        this.FontChanged += new System.EventHandler(this.WaterMark_FontChanged);
-        //No one of the above events will start immeddiatlly 
+        TextChanged += WaterMark_Toggle;
+        LostFocus += WaterMark_Toggle;
+        FontChanged += WaterMark_FontChanged;
+
+        //No one of the above events will start immediately 
         //TextBox control still in constructing, so,
         //Font object (for example) couldn't be catched from within WaterMark_Toggle
-        //So, call WaterMark_Toggel through OnCreateControl after TextBox is totally created
+        //So, call WaterMark_Toggle through OnCreateControl after TextBox is totally created
         //No doupt, it will be only one time call
 
         //Old solution uses Timer.Tick event to check Create property
       }
     }
 
-    private void WaterMark_Toggel(object sender, EventArgs args)
+    private void WaterMark_Toggle(object sender, EventArgs args)
     {
-      if (this.Text.Length <= 0)
+      if (Text.Length <= 0)
+      {
         EnableWaterMark();
+      }
       else
-        DisbaleWaterMark();
+      {
+        DisableWaterMark();
+      }
     }
 
     private void EnableWaterMark()
     {
       //Save current font until returning the UserPaint style to false (NOTE: It is a try and error advice)
-      oldFont = new System.Drawing.Font(Font.FontFamily, Font.Size, Font.Style, Font.Unit);
+      oldFont = new Font(Font.FontFamily, Font.Size, Font.Style, Font.Unit);
+
       //Enable OnPaint event handler
-      this.SetStyle(ControlStyles.UserPaint, true);
-      this.waterMarkTextEnabled = true;
+      SetStyle(ControlStyles.UserPaint, true);
+
+      waterMarkTextEnabled = true;
+
       //Triger OnPaint immediatly
       Refresh();
     }
 
-    private void DisbaleWaterMark()
+    private void DisableWaterMark()
     {
       //Disbale OnPaint event handler
-      this.waterMarkTextEnabled = false;
-      this.SetStyle(ControlStyles.UserPaint, false);
+      waterMarkTextEnabled = false;
+
+      SetStyle(ControlStyles.UserPaint, false);
+
       //Return back oldFont if existed
       if (oldFont != null)
-        this.Font = new System.Drawing.Font(oldFont.FontFamily, oldFont.Size, oldFont.Style, oldFont.Unit);
+      {
+        Font = new Font(oldFont.FontFamily, oldFont.Size, oldFont.Style, oldFont.Unit);
+      }
     }
 
     private void WaterMark_FontChanged(object sender, EventArgs args)
     {
       if (waterMarkTextEnabled)
       {
-        oldFont = new System.Drawing.Font(Font.FontFamily, Font.Size, Font.Style, Font.Unit);
+        oldFont = new Font(Font.FontFamily, Font.Size, Font.Style, Font.Unit);
         Refresh();
       }
     }
