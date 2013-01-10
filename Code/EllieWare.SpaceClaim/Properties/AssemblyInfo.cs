fa2a59873d712936e2ba9d30c2ad17cc6f2c82b6
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-10 17:20:58 +0000

    - added copyright info to SpaceClaim

--- Code/EllieWare.SpaceClaim/Properties/AssemblyInfo.cs
+++ Code/EllieWare.SpaceClaim/Properties/AssemblyInfo.cs
@@ -6,9 +6,12 @@
 //  www.EllieWare.com
 //
 using System.Reflection;
+using EllieWare.Interfaces;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EllieWare.SpaceClaim")]
 [assembly: AssemblyProduct("EllieWare.SpaceClaim")]
+
+[assembly: LicenseInformation("Copyright.txt")]
