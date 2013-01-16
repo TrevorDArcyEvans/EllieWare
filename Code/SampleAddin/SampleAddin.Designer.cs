7cb8d484ecc2299097db20cfb986a98002c75c92
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-16 17:20:40 +0000

    - added sample addin project

---
+++ Code/SampleAddin/SampleAddin.Designer.cs
@@ -0,0 +1,98 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+namespace SampleAddin
+{
+  partial class SampleAddin
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+      System.Windows.Forms.Label label1;
+      this.mText = new System.Windows.Forms.TextBox();
+      tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      label1 = new System.Windows.Forms.Label();
+      tableLayoutPanel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // tableLayoutPanel1
+      // 
+      tableLayoutPanel1.ColumnCount = 2;
+      tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+      tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      tableLayoutPanel1.Controls.Add(label1, 0, 0);
+      tableLayoutPanel1.Controls.Add(this.mText, 1, 0);
+      tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+      tableLayoutPanel1.Name = "tableLayoutPanel1";
+      tableLayoutPanel1.RowCount = 1;
+      tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      tableLayoutPanel1.Size = new System.Drawing.Size(385, 281);
+      tableLayoutPanel1.TabIndex = 0;
+      // 
+      // label1
+      // 
+      label1.AutoSize = true;
+      label1.Location = new System.Drawing.Point(3, 6);
+      label1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
+      label1.Name = "label1";
+      label1.Size = new System.Drawing.Size(53, 13);
+      label1.TabIndex = 0;
+      label1.Text = "Message:";
+      // 
+      // mText
+      // 
+      this.mText.Dock = System.Windows.Forms.DockStyle.Top;
+      this.mText.Location = new System.Drawing.Point(62, 3);
+      this.mText.Name = "mText";
+      this.mText.Size = new System.Drawing.Size(320, 20);
+      this.mText.TabIndex = 1;
+      this.mText.Text = "Hello, world!";
+      this.mText.TextChanged += new System.EventHandler(this.Text_TextChanged);
+      // 
+      // SampleAddin
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(tableLayoutPanel1);
+      this.Name = "SampleAddin";
+      this.Size = new System.Drawing.Size(385, 281);
+      tableLayoutPanel1.ResumeLayout(false);
+      tableLayoutPanel1.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox mText;
+  }
+}
