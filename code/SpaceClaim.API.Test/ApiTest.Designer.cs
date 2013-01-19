258e172396259c18aa7181c59e35ad1e590ab6d8
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-19 21:11:52 +0000

    - changed to use a reflection based method to populate dropdown list of tests

--- code/SpaceClaim.API.Test/ApiTest.Designer.cs
+++ code/SpaceClaim.API.Test/ApiTest.Designer.cs
@@ -36,12 +36,13 @@ namespace SpaceClaim.API.Test
     private void InitializeComponent()
     {
       System.Windows.Forms.Button CmdTest;
+      this.ApiTests = new System.Windows.Forms.ComboBox();
       CmdTest = new System.Windows.Forms.Button();
       this.SuspendLayout();
       // 
       // CmdTest
       // 
-      CmdTest.Location = new System.Drawing.Point(97, 81);
+      CmdTest.Location = new System.Drawing.Point(95, 39);
       CmdTest.Name = "CmdTest";
       CmdTest.Size = new System.Drawing.Size(75, 23);
       CmdTest.TabIndex = 0;
@@ -49,20 +50,33 @@ namespace SpaceClaim.API.Test
       CmdTest.UseVisualStyleBackColor = true;
       CmdTest.Click += new System.EventHandler(this.CmdTest_Click);
       // 
+      // ApiTests
+      // 
+      this.ApiTests.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.ApiTests.FormattingEnabled = true;
+      this.ApiTests.Location = new System.Drawing.Point(12, 12);
+      this.ApiTests.Name = "ApiTests";
+      this.ApiTests.Size = new System.Drawing.Size(240, 21);
+      this.ApiTests.Sorted = true;
+      this.ApiTests.TabIndex = 1;
+      // 
       // ApiTest
       // 
       this.AcceptButton = CmdTest;
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(264, 209);
+      this.ClientSize = new System.Drawing.Size(264, 76);
+      this.Controls.Add(this.ApiTests);
       this.Controls.Add(CmdTest);
       this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
       this.Name = "ApiTest";
-      this.Text = "ApiTest";
+      this.Text = "EllieWare API Tester";
       this.ResumeLayout(false);
 
     }
 
     #endregion
+
+    private System.Windows.Forms.ComboBox ApiTests;
   }
 }
--- code/SpaceClaim.API.Test/ApiTest.cs
+++ code/SpaceClaim.API.Test/ApiTest.cs
@@ -6,6 +6,9 @@
 //  www.EllieWare.com
 //
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
 using SpaceClaim.Api.V10;
 
@@ -13,20 +16,38 @@ namespace SpaceClaim.API.Test
 {
   public partial class ApiTest : Form
   {
+    private readonly Dictionary<string, MethodInfo> mTestMethods;
+
     public ApiTest()
     {
       InitializeComponent();
+
+      ApiTests.Items.Clear();
+
+      mTestMethods = Assembly.GetExecutingAssembly()
+                        .GetTypes()
+                        .SelectMany(x => x.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance))
+                        .Where(y => y.GetCustomAttributes(false).OfType<SpaceClaimTestAttribute>().Any())
+                        .ToDictionary(z => z.Name);
+      ApiTests.Items.AddRange(mTestMethods.Keys.ToArray());
+
+      if (ApiTests.Items.Count > 0)
+      {
+        ApiTests.SelectedIndex = 0;
+      }
     }
 
     private void CmdTest_Click(object sender, EventArgs e)
     {
-      TestReload();
+      var mi = mTestMethods[(string)ApiTests.SelectedItem];
+      mi.Invoke(this, null);
     }
 
     #region TestReload
 
     private string mFilePath;
 
+    [SpaceClaimTestAttribute]
     private void TestReload()
     {
       WriteBlock.AppendTask(() =>
