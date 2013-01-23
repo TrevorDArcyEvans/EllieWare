35fcdf7ea2d9cae598378e1fe664731b246ccf5d
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-23 11:22:15 +0000

    - added clear selection - added add selection to an existing group

---
+++ code/EllieWare.SpaceClaim/AddSelectionToGroup.Designer.cs
@@ -0,0 +1,99 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+
+namespace EllieWare.SpaceClaim
+{
+  partial class AddSelectionToGroup
+  {
+
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+      System.Windows.Forms.Label label1;
+      this.SelectedGroup = new System.Windows.Forms.ComboBox();
+      tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      label1 = new System.Windows.Forms.Label();
+      tableLayoutPanel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // tableLayoutPanel1
+      // 
+      tableLayoutPanel1.ColumnCount = 2;
+      tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+      tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      tableLayoutPanel1.Controls.Add(label1, 0, 0);
+      tableLayoutPanel1.Controls.Add(this.SelectedGroup, 1, 0);
+      tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+      tableLayoutPanel1.Name = "tableLayoutPanel1";
+      tableLayoutPanel1.RowCount = 1;
+      tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      tableLayoutPanel1.Size = new System.Drawing.Size(266, 104);
+      tableLayoutPanel1.TabIndex = 0;
+      // 
+      // label1
+      // 
+      label1.AutoSize = true;
+      label1.Location = new System.Drawing.Point(3, 6);
+      label1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
+      label1.Name = "label1";
+      label1.Size = new System.Drawing.Size(39, 13);
+      label1.TabIndex = 0;
+      label1.Text = "Group:";
+      // 
+      // SelectedGroup
+      // 
+      this.SelectedGroup.Dock = System.Windows.Forms.DockStyle.Top;
+      this.SelectedGroup.FormattingEnabled = true;
+      this.SelectedGroup.Location = new System.Drawing.Point(48, 3);
+      this.SelectedGroup.Name = "SelectedGroup";
+      this.SelectedGroup.Size = new System.Drawing.Size(215, 21);
+      this.SelectedGroup.TabIndex = 1;
+      this.SelectedGroup.SelectedIndexChanged += new System.EventHandler(this.SelectedGroup_SelectedIndexChanged);
+      // 
+      // AddSelectionToGroup
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.Controls.Add(tableLayoutPanel1);
+      this.Name = "AddSelectionToGroup";
+      this.Size = new System.Drawing.Size(266, 104);
+      tableLayoutPanel1.ResumeLayout(false);
+      tableLayoutPanel1.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ComboBox SelectedGroup;
+  }
+}
---
+++ code/EllieWare.SpaceClaim/AddSelectionToGroupFactory.cs
@@ -0,0 +1,65 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System;
+using System.Collections.Generic;
+using EllieWare.Interfaces;
+
+namespace EllieWare.SpaceClaim
+{
+  public class AddSelectionToGroupFactory : IFactory
+  {
+    public string Title
+    {
+      get
+      {
+        return "Add the current selection(s) to an existing group";
+      }
+    }
+
+    public string Description
+    {
+      get
+      {
+        return "Add the current selection(s) to an existing group";
+      }
+    }
+
+    public string Keywords
+    {
+      get
+      {
+        return "SpaceClaim, selection";
+      }
+    }
+
+    public IEnumerable<string> Categories
+    {
+      get
+      {
+        return new[]
+                     {
+                       "SpaceClaim"
+                     };
+      }
+    }
+
+    public Type CreatedType
+    {
+      get
+      {
+        return typeof(AddSelectionToGroup);
+      }
+    }
+
+    public IRunnable Create(IRobotWare root, ICallback callback, IParameterManager mgr)
+    {
+      return new AddSelectionToGroup(root, callback, mgr);
+    }
+  
+  }
+}
