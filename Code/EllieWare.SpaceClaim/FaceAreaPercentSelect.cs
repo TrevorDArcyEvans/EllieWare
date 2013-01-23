93aa111047f936b52cdc1766e6f497a4d911d7f4
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-23 23:04:33 +0000

    - added face area percent select

---
+++ Code/EllieWare.SpaceClaim/FaceAreaPercentSelect.cs
@@ -0,0 +1,43 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System.Collections.Generic;
+using EllieWare.Interfaces;
+using SpaceClaim.Api.V10;
+
+namespace EllieWare.SpaceClaim
+{
+  public class FaceAreaPercentSelect : FaceAreaPercentBase
+  {
+    public FaceAreaPercentSelect() :
+      base()
+    {
+      ColorLabel.Visible = ColorSwatch.Visible = false;
+    }
+
+    public FaceAreaPercentSelect(IRobotWare root, ICallback callback, IParameterManager mgr) :
+      base(root, callback, mgr)
+    {
+      ColorLabel.Visible = ColorSwatch.Visible = false;
+    }
+
+    public override string Summary
+    {
+      get
+      {
+        var descrip = string.Format("Select all faces below {0}% of the largest face", AreaThreshold.Value);
+
+        return descrip;
+      }
+    }
+
+    protected override bool ProcessFaces(Dictionary<DesignBody, IEnumerable<DesignFace>> smallFaces)
+    {
+      return SelectFaces(smallFaces);
+    }
+  }
+}
---
+++ Code/EllieWare.SpaceClaim/FaceAreaPercentSelectFactory.cs
@@ -0,0 +1,64 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System;
+using System.Collections.Generic;
+using EllieWare.Interfaces;
+
+namespace EllieWare.SpaceClaim
+{
+  public class FaceAreaPercentSelectFactory : IFactory
+  {
+    public string Title
+    {
+      get
+      {
+        return "Select all faces below a fraction of the largest face";
+      }
+    }
+
+    public string Description
+    {
+      get
+      {
+        return "Select all faces below a fraction of the largest face";
+      }
+    }
+
+    public string Keywords
+    {
+      get
+      {
+        return "SpaceClaim, area, geometry";
+      }
+    }
+
+    public IEnumerable<string> Categories
+    {
+      get
+      {
+        return new[]
+                     {
+                       "SpaceClaim"
+                     };
+      }
+    }
+
+    public Type CreatedType
+    {
+      get
+      {
+        return typeof(FaceAreaPercentSelect);
+      }
+    }
+
+    public IRunnable Create(IRobotWare root, ICallback callback, IParameterManager mgr)
+    {
+      return new FaceAreaPercentSelect(root, callback, mgr);
+    }
+  }
+}
