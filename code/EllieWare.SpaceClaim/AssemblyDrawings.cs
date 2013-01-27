56ece43160d4bdb13278d598785f21fc9b78a167
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-27 13:25:31 +0000

    - added drawings from assy

---
+++ code/EllieWare.SpaceClaim/AssemblyDrawings.cs
@@ -0,0 +1,134 @@
+﻿//
+//  Copyright (C) 2012 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using EllieWare.Interfaces;
+using SpaceClaim.Api.V10;
+using SpaceClaim.Api.V10.Geometry;
+
+namespace EllieWare.SpaceClaim
+{
+  public partial class AssemblyDrawings : SpaceClaimMutableRunnableBase
+  {
+    private readonly List<KeyValuePair<string, DrawingViewStyle>> SupportedViewStyles = new List<KeyValuePair<string, DrawingViewStyle>>
+                                                                                    {
+                                                                                      new KeyValuePair<string, DrawingViewStyle >("Shaded", DrawingViewStyle.Shaded),
+                                                                                      new KeyValuePair<string, DrawingViewStyle >("Wire Frame", DrawingViewStyle.WireFrame),
+                                                                                      new KeyValuePair<string, DrawingViewStyle >("Hidden Edges Dashed", DrawingViewStyle.HiddenEdgesDashed),
+                                                                                      new KeyValuePair<string, DrawingViewStyle >("Hidden Edges Removed", DrawingViewStyle.HiddenEdgesRemoved)
+                                                                                    };
+
+    private string TemplateDir = @"C:\Program Files\SpaceClaim\Library\DrawingFormats";
+    private const string SpaceClaimFileExtn = ".scdoc";
+
+    public AssemblyDrawings() :
+      base()
+    {
+      InitializeComponent();
+    }
+
+    public AssemblyDrawings(IRobotWare root, ICallback callback, IParameterManager mgr) :
+      base(root, callback, mgr)
+    {
+      InitializeComponent();
+
+      // populate templates
+      var templates = from thisTemplate in Directory.EnumerateFiles(TemplateDir, "*" + SpaceClaimFileExtn)
+                      select Path.GetFileNameWithoutExtension(thisTemplate);
+      Template.DataSource = templates.ToList();
+      if (Template.Items.Count > 0)
+      {
+        Template.SelectedIndex = 0;
+      }
+
+      // populate drawing view styles
+      DrawingStyle.Items.Clear();
+
+      foreach (var kvp in SupportedViewStyles)
+      {
+        DrawingStyle.Items.Add(kvp.Key);
+      }
+      DrawingStyle.SelectedIndex = 0;
+    }
+
+    public override string Summary
+    {
+      get
+      {
+        var descrip = string.Format("Create drawings from the current assembly");
+
+        return descrip;
+      }
+    }
+
+    public override void ReadXml(XmlReader reader)
+    {
+      var templateStr = reader.GetAttribute("Template");
+      var index = Template.Items.IndexOf(templateStr);
+      Template.SelectedIndex = index;
+
+      var numStr = reader.GetAttribute("DrawingStyle");
+      var num = int.Parse(numStr, NumberStyles.Integer, CultureInfo.InvariantCulture);
+      DrawingStyle.SelectedIndex = num;
+    }
+
+    public override void WriteXml(XmlWriter writer)
+    {
+      writer.WriteAttributeString("Template", (string)Template.SelectedItem);
+      writer.WriteAttributeString("DrawingStyle", DrawingStyle.SelectedIndex.ToString(CultureInfo.InvariantCulture));
+    }
+
+    private void ProcessPart(Document doc, Part part, DrawingSheet sheetTemplate)
+    {
+      var sheet = DrawingSheet.Create(doc, sheetTemplate);
+      var sheetMajorDim = Math.Max(sheet.Width, sheet.Height);
+      var frontOrigin = PointUV.Create(0.1 * sheetMajorDim, 0.1 * sheetMajorDim);
+      var viewFront = DrawingView.CreateGeneralView(sheet, part, ViewProjection.Front, frontOrigin);
+      var frontBox = viewFront.Extent;
+      var viewTop = DrawingView.CreateProjectedView(viewFront, PointUV.Create(frontBox.Center.U, frontBox.Center.V + frontBox.RangeV.Span / 1d));
+      var viewRight = DrawingView.CreateProjectedView(viewFront, PointUV.Create(frontBox.Center.U + frontBox.RangeU.Span / 1d, frontBox.Center.V));
+
+      var isoOrigin = PointUV.Create(0.75 * sheetMajorDim, 0.75 * sheetMajorDim);
+      var viewIso = DrawingView.CreateGeneralView(sheet, part, ViewProjection.Isometric, isoOrigin);
+
+      var drawingStyle = SupportedViewStyles[DrawingStyle.SelectedIndex].Value;
+      viewFront.Style = viewTop.Style = viewRight.Style = viewIso.Style = drawingStyle;
+    }
+
+    protected override bool DoRun()
+    {
+      var templatePath = Path.Combine(TemplateDir, (string)Template.SelectedItem);
+      var templateFilePath = Path.ChangeExtension(templatePath, SpaceClaimFileExtn);
+      var templateDoc = Document.Load(templateFilePath);
+      var templateSheet = templateDoc.DrawingSheets.First();
+
+      var doc = Window.ActiveWindow.Document;
+      var parts = doc.Parts;
+      foreach (var part in parts.Where(p => !p.Equals(doc.MainPart) && p.Components.Count == 0))
+      {
+        ProcessPart(doc, part, templateSheet);
+      }
+
+      return true;
+    }
+
+    private void Template_SelectedIndexChanged(object sender, System.EventArgs e)
+    {
+      FireConfigurationChanged();
+    }
+
+    private void DrawingStyle_SelectedIndexChanged(object sender, System.EventArgs e)
+    {
+      FireConfigurationChanged();
+    }
+  }
+}
---
+++ code/EllieWare.SpaceClaim/AssemblyDrawingsFactory.cs
@@ -0,0 +1,64 @@
+﻿//
+//  Copyright (C) 2012 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System;
+using System.Collections.Generic;
+using EllieWare.Interfaces;
+
+namespace EllieWare.SpaceClaim
+{
+  public class AssemblyDrawingsFactory : IFactory
+  {
+    public string Title
+    {
+      get
+      {
+        return "Create drawings from the current assembly";
+      }
+    }
+
+    public string Description
+    {
+      get
+      {
+        return "Create drawings from the current assembly";
+      }
+    }
+
+    public string Keywords
+    {
+      get
+      {
+        return "SpaceClaim, drawing, assembly";
+      }
+    }
+
+    public IEnumerable<string> Categories
+    {
+      get
+      {
+        return new[]
+                     {
+                       "SpaceClaim"
+                     };
+      }
+    }
+
+    public Type CreatedType
+    {
+      get
+      {
+        return typeof(AssemblyDrawings);
+      }
+    }
+
+    public IRunnable Create(IRobotWare root, ICallback callback, IParameterManager mgr)
+    {
+      return new AssemblyDrawings(root, callback, mgr);
+    }
+  }
+}
