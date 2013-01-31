68e19b659beec04573c820e302de2bc23a153f69
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2013-01-31 16:13:23 +0000

    - added colour/select/remove faces based on curvature threshold

---
+++ Code/EllieWare.SpaceClaim/FaceCurvatureColor.cs
@@ -0,0 +1,43 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System.Collections.Generic;
+using EllieWare.Interfaces;
+using SpaceClaim.Api.V10;
+
+namespace EllieWare.SpaceClaim
+{
+  public class FaceCurvatureColor : FaceCurvatureBase
+  {
+    public FaceCurvatureColor()
+    {
+    }
+
+    public FaceCurvatureColor(IRobotWare root, ICallback callback, IParameterManager mgr) :
+      base(root, callback, mgr)
+    {
+    }
+
+    public override string Summary
+    {
+      get
+      {
+        var descrip = string.Format("Change the color of all faces with a curvature below {0} {1} to {2}",
+                        AreaThreshold.Value,
+                        (Window.ActiveWindow != null) ? Window.ActiveWindow.Units.Length.Symbol : "mm",
+                        ColorDlg.Color);
+
+        return descrip;
+      }
+    }
+
+    protected override bool ProcessFaces(Dictionary<DesignBody, IEnumerable<DesignFace>> smallFaces)
+    {
+      return ColorFaces(smallFaces);
+    }
+  }
+}
---
+++ Code/EllieWare.SpaceClaim/FaceCurvatureSelect.cs
@@ -0,0 +1,44 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System.Collections.Generic;
+using EllieWare.Interfaces;
+using SpaceClaim.Api.V10;
+
+namespace EllieWare.SpaceClaim
+{
+  public class FaceCurvatureSelect : FaceCurvatureBase
+  {
+    public FaceCurvatureSelect()
+    {
+      ColorLabel.Visible = ColorSwatch.Visible = false;
+    }
+
+    public FaceCurvatureSelect(IRobotWare root, ICallback callback, IParameterManager mgr) :
+      base(root, callback, mgr)
+    {
+      ColorLabel.Visible = ColorSwatch.Visible = false;
+    }
+
+    public override string Summary
+    {
+      get
+      {
+        var descrip = string.Format("Select all faces with a curvature below {0} {1}",
+                        AreaThreshold.Value,
+                        (Window.ActiveWindow != null) ? Window.ActiveWindow.Units.Length.Symbol : "mm");
+
+        return descrip;
+      }
+    }
+
+    protected override bool ProcessFaces(Dictionary<DesignBody, IEnumerable<DesignFace>> smallFaces)
+    {
+      return SelectFaces(smallFaces);
+    }
+  }
+}
