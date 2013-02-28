f5c34a6952c57ff399dab8c0fb194b935165bead
trevorde <tdarcyevans@hotmail.com>
2013-02-28 15:45:33 +0000

    - added arbitrarily oriented, minimal LAR size

--- Consulting/SheetMetal/Code/SheetMetalEstimator/SheetMetalEstimator.cs
+++ Consulting/SheetMetal/Code/SheetMetalEstimator/SheetMetalEstimator.cs
@@ -7,6 +7,7 @@
 //
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -18,6 +19,7 @@ using EllieWare.Interfaces;
 using SpaceClaim.Api.V10;
 using SpaceClaim.Api.V10.Geometry;
 using SpaceClaim.Api.V10.Modeler;
+using Point = SpaceClaim.Api.V10.Geometry.Point;
 
 namespace SheetMetalEstimator
 {
@@ -95,6 +97,90 @@ namespace SheetMetalEstimator
       return allFlatParts.SingleOrDefault();
     }
 
+    private SurfaceEvaluation EvalCentre(Face face)
+    {
+      var surface = face.Geometry;
+      var box = face.BoxUV;
+      var centre = box.Center;
+      var surfEval = surface.Evaluate(centre);
+
+      return surfEval;
+    }
+
+    private Point GetCentrePoint(Face face)
+    {
+      var centre = EvalCentre(face).Point;
+
+      return centre;
+    }
+
+    private Direction GetCentreNormal(Face face)
+    {
+      var normal = EvalCentre(face).Normal;
+
+      return normal;
+    }
+
+    // returns size of a arbitrarily oriented, minimal bounding box for the face
+    private SizeF GetMinimalBoundingBoxSize(Face face)
+    {
+      var origin = GetCentrePoint(face);
+      var normal = GetCentreNormal(face);
+      var frame = Frame.Create(origin, normal);
+      var proj = Matrix.CreateMapping(frame);
+      var projInv = proj.Inverse;
+      var line = Line.Create(origin, normal);
+      var minArea = Double.MaxValue;
+
+      var minBox = Box.Create(Point.Origin);
+      for (var i = 0; i <= 180; i += 5)
+      {
+        var angleRad = i / Math.PI / 180;
+        var rotation = Matrix.CreateRotation(line, angleRad);
+        var bbox = face.GetBoundingBox(rotation);
+        var invBbox = projInv * bbox;
+        var area = invBbox.Size.X * invBbox.Size.Y;
+        if (area < minArea)
+        {
+          minArea = area;
+          minBox = invBbox;
+        }
+      }
+
+      return new SizeF((float)minBox.Size.X, (float)minBox.Size.Y);
+    }
+
+    private bool CreateFlatPatternDXF(string dxfFilePath, Body firstBody)
+    {
+      var largestFace = firstBody.Faces.OrderBy(x => x.Area).Last();
+      var tempDoc = Document.Create();
+      var tempPart = tempDoc.MainPart;
+      var tempMasterUnused = DesignBody.Create(tempPart, "Flat Pattern", firstBody);
+      var largestSurface = largestFace.Geometry;
+      var box = largestFace.BoxUV;
+      var centre = box.Center;
+      var surfEval = largestSurface.Evaluate(centre);
+      var normal = surfEval.Normal;
+      var origin = surfEval.Point;
+      var frame = Frame.Create(origin, normal);
+      var viewProj = Matrix.CreateMapping(frame);
+      var viewProjInv = viewProj.Inverse;
+      var firstTempWindow = Window.GetWindows(tempDoc).First();
+
+      firstTempWindow.SetProjection(viewProjInv, true, false);
+
+      // this is the API causing the problems :-(
+      //firstTempWindow.Export(WindowExportFormat.AutoCadDxf, dxfFilePath);
+
+      //var allTempWindows = Window.GetWindows(tempDoc);
+      //foreach (var thisWindow in allTempWindows)
+      //{
+      //  thisWindow.Close();
+      //}
+
+      return true;
+    }
+
     private bool DoRun()
     {
       var doc = Window.ActiveWindow.Document;
@@ -117,7 +203,7 @@ namespace SheetMetalEstimator
       var orderedFaces = firstBody.Faces.OrderBy(x => x.Area).ToList();
       var largestFace = orderedFaces[orderedFaces.Count - 1];
       var opositeLargestFace = orderedFaces[orderedFaces.Count - 2];
-      var bbox = largestFace.GetBoundingBox(Matrix.Identity);
+      var larSize = GetMinimalBoundingBoxSize(largestFace);
       var separation = largestFace.GetClosestSeparation(opositeLargestFace);
       var thickness = separation.Distance;
       var lengthUnit = doc.Units.Length;
@@ -133,8 +219,8 @@ namespace SheetMetalEstimator
                               flatPattern.BendFaces.Count(),
                               numHoles,
                               Math.Round(thickness * lengthFactor, 2),
-                              Math.Round(bbox.Size.X * lengthFactor, 1),
-                              Math.Round(bbox.Size.Y * lengthFactor, 1));
+                              Math.Round(larSize.Width * lengthFactor, 1),
+                              Math.Round(larSize.Height * lengthFactor, 1));
       var path = ExcelFilePath.Text;
 
       if (!File.Exists(path))
@@ -156,35 +242,6 @@ namespace SheetMetalEstimator
       {
         var dxfFilePath = Path.ChangeExtension(doc.Path, ".dxf");
         CreateFlatPatternDXF(dxfFilePath, firstBody);
-      }
-
-      return true;
-    }
-
-    private bool CreateFlatPatternDXF(string dxfFilePath, Body firstBody)
-    {
-      var largestFace = firstBody.Faces.OrderBy(x => x.Area).Last();
-      var tempDoc = Document.Create();
-      var tempPart = tempDoc.MainPart;
-      var tempMasterUnused = DesignBody.Create(tempPart, "Flat Pattern", firstBody);
-      var largestSurface = largestFace.Geometry;
-      var box = largestFace.BoxUV;
-      var centre = box.Center;
-      var surfEval = largestSurface.Evaluate(centre);
-      var normal = surfEval.Normal;
-      var origin = surfEval.Point;
-      var frame = Frame.Create(origin, normal);
-      var viewProj = Matrix.CreateMapping(frame);
-      var viewProjInv = viewProj.Inverse;
-      var firstTempWindow = Window.GetWindows(tempDoc).First();
-
-      firstTempWindow.SetProjection(viewProjInv, true, false);
-      firstTempWindow.Export(WindowExportFormat.AutoCadDxf, dxfFilePath);
-
-      var allTempWindows = Window.GetWindows(tempDoc);
-      foreach (var thisWindow in allTempWindows)
-      {
-        thisWindow.Close();
       }
 
       return true;
