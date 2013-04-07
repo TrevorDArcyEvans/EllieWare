e1d8a32579a14822f60b4243abae7b163a096aab
trevorde <tdarcyevans@hotmail.com>
2013-04-07 22:08:00 +0100

    - initial SpaceClaim integration

---
+++ code/EllieWare.Python/Python.SpaceClaim.cs
@@ -0,0 +1,56 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using EllieWare.Python.Properties;
+using SpaceClaim.Api.V10.Extensibility;
+
+namespace EllieWare.Python
+{
+  public class EllieWarePythonAddin : AddIn, IExtensibility, ICommandExtensibility, IRibbonExtensibility
+  {
+    private readonly CommandCapsule[] mCapsules = new[]
+                                                   {
+                                                     new CommandCapsule("EllieWare.Python.RibbonTab", "EllieWare Python"),
+                                                     new CommandCapsule("EllieWare.Python.ManagerGroup", "Python"),
+                                                     new PythonCapsule()
+                                                   };
+
+    #region Implementation of IExtensibility
+
+    public bool Connect()
+    {
+      return true;
+    }
+
+    public void Disconnect()
+    {
+    }
+
+    #endregion
+
+    #region Implementation of ICommandExtensibility
+
+    public void Initialize()
+    {
+      foreach (var commandCapsule in mCapsules)
+      {
+        commandCapsule.Initialize();
+      }
+    }
+
+    #endregion
+
+    #region Implementation of IRibbonExtensibility
+
+    public string GetCustomUI()
+    {
+      return Resources.Ribbon;
+    }
+
+    #endregion
+  }
+}
---
+++ code/EllieWare.Python/PythonCapsule.cs
@@ -0,0 +1,33 @@
+﻿//
+//  Copyright (C) 2013 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System.Drawing;
+using EllieWare.Python.Properties;
+using SpaceClaim.Api.V10;
+using SpaceClaim.Api.V10.Extensibility;
+
+namespace EllieWare.Python
+{
+  public class PythonCapsule : CommandCapsule
+  {
+    public PythonCapsule()
+      : base("EllieWare.Python.Console", "Python", Resources.python_32x32, "Run a Python script")
+    {
+    }
+
+    protected override void OnUpdate(Command command)
+    {
+      command.IsEnabled = true;
+    }
+
+    protected sealed override void OnExecute(Command command, ExecutionContext context, Rectangle buttonRect)
+    {
+      var dlg = new PyConsole();
+      dlg.ShowDialog();
+    }
+  }
+}
