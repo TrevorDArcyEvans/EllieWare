1b56caadd4b13faf71fa44e7cb65af6a119cc150
trevorde <tdarcyevans@hotmail.com>
2013-04-10 21:58:44 +0100

    - IronPython licenses

--- code/SerpentWare.Common/Properties/AssemblyInfo.cs
+++ code/SerpentWare.Common/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿//
+//
 //  Copyright (C) 2013 EllieWare
 //
 //  All rights reserved
@@ -6,9 +6,13 @@
 //  www.EllieWare.com
 //
 using System.Reflection;
+using EllieWare.Interfaces;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SerpentWare.Common")]
 [assembly: AssemblyProduct("SerpentWare.Common")]
+
+[assembly: LicenseInformation("IronPython-LICENSE1.txt")]
+[assembly: LicenseInformation("IronPython-License2.txt")]
