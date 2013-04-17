8722b2d261b4387c2739948d187de40c90801e57
trevorde <tdarcyevans@hotmail.com>
2013-04-18 00:13:46 +0100

    - added SerpentWare specific common assy info

---
+++ Code/SW_AssemblyInfoCommon.cs
@@ -0,0 +1,37 @@
+//
+//  Copyright (C) 2012 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("SerpentWare by EllieWare. IronPython for everyone.")]
+[assembly: AssemblyCompany("EllieWare")]
+[assembly: AssemblyCopyright("Copyright © EllieWare 2012")]
+[assembly: AssemblyTrademark("SerpentWare is a trademark of EllieWare")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
