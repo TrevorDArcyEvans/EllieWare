6d78d00cb54cd286c1b0aae44be78469c1e699dd
unknown <trevor.darcy-evans@UKCAMD5042.aveva.com>
2014-05-30 13:49:47 +0100

    - added Runtime.Server -- stubbed out support for Quartz.Net

---
+++ Code/EllieWare.Runtime.Server/Host.cs
@@ -0,0 +1,18 @@
+﻿//
+//  Copyright (C) 2014 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EllieWare.Runtime.Server
+{
+  public class Host
+  {
+  }
+}
