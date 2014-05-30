034869d97633d1a4b3b611499ee6009876f0c912
unknown <trevor.darcy-evans@UKCAMD5042.aveva.com>
2014-05-30 15:15:22 +0100

    - added C5

---
+++ Code/C5/C5.UserGuideExamples/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// This file is part of the C5 Generic Collection Library for C# and CLI
+// See https://github.com/sestoft/C5/blob/master/LICENSE.txt for licensing details.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("C5.UserGuideExamples")]
+[assembly: AssemblyDescription("User guide examples for C5")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Niels Kokholm, Peter Sestoft, Rasmus Nielsen")]
+[assembly: AssemblyProduct("C5")]
+[assembly: AssemblyCopyright("Copyright © Niels Kokholm, Peter Sestoft, and Rasmus Nielsen 2003-2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("2.2.*")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9cc9b3be-ff81-4007-b27f-a4a75a91aa3a")]
