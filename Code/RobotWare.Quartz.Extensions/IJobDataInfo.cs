9ccf29e90d0b11a8d817253db74cfdaf32980b16
unknown <trevor.darcy-evans@UKCAMD5042.aveva.com>
2014-06-05 14:34:55 +0100

    - added RobotWare.Quartz.Extensions

---
+++ Code/RobotWare.Quartz.Extensions/IJobDataInfo.cs
@@ -0,0 +1,40 @@
+﻿//
+//  Copyright (C) 2014 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System;
+using Quartz;
+
+namespace RobotWare.Quartz.Extensions
+{
+  public interface IJobDataInfo
+  {
+    /// <summary>
+    /// Name of parameter in <see cref="JobDataMap"/>
+    /// </summary>
+    string Name { get; }
+
+    /// <summary>
+    /// Type of object in <see cref="JobDataMap"/>
+    /// </summary>
+    Type Type { get; }
+
+    /// <summary>
+    /// Type of editor to change the parameter
+    /// <remarks>
+    /// This may be the same as <seealso cref="Type"/>
+    /// This is intended to be used as a hint so that clients can
+    /// provide an appropriate user interface to edit the parameter.
+    /// </remarks>
+    /// </summary>
+    Type EditorType { get; }
+
+    /// <summary>
+    /// Textual description of parameter in <see cref="JobDataMap"/>
+    /// </summary>
+    string Description { get; }
+  }
+}
---
+++ Code/RobotWare.Quartz.Extensions/IJobInfo.cs
@@ -0,0 +1,22 @@
+﻿//
+//  Copyright (C) 2014 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System.Collections.Generic;
+using Quartz;
+
+namespace RobotWare.Quartz.Extensions
+{
+  public interface IJobInfo : IJob
+  {
+    /// <summary>
+    /// read-only list of <see cref="IJobDataInfo"/> so that clients
+    /// can provide an appropriate user interface for creating/editing
+    /// job parameters
+    /// </summary>
+    IEnumerable<IJobDataInfo> JobDataInfos { get; }
+  }
+}
