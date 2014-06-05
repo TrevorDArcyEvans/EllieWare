1ad29f4e58de57a0bc48c7708761020a0046367d
unknown <trevor.darcy-evans@UKCAMD5042.aveva.com>
2014-06-05 16:22:35 +0100

    - added concrete implementation of IJobDataInfo

---
+++ Code/RobotWare.Quartz.Extensions/JobDataInfo.cs
@@ -0,0 +1,27 @@
+﻿//
+//  Copyright (C) 2014 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+using System;
+
+namespace RobotWare.Quartz.Extensions
+{
+  public class JobDataInfo : IJobDataInfo
+  {
+    public JobDataInfo(string name, Type type, Type editorType, string descrip)
+    {
+      Name = name;
+      DataType = type;
+      EditorType = editorType;
+      Description = descrip;
+    }
+
+    public string Name { get; private set; }
+    public Type DataType { get; private set; }
+    public Type EditorType { get; private set; }
+    public string Description { get; private set; }
+  }
+}
