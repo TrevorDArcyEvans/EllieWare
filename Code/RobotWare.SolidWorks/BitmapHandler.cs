36c5139d3b84eb69da83b382161a915fe9bd94cd
Trevor D'Arcy-Evans <trevor.darcy-evans@aveva.com>
2014-11-13 13:21:08 +0000

    - bitmap handler based on SolidWorks code

---
+++ Code/RobotWare.SolidWorks/BitmapHandler.cs
@@ -0,0 +1,62 @@
+﻿//
+//  Copyright (C) 2014 EllieWare
+//
+//  All rights reserved
+//
+//  www.EllieWare.com
+//
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+namespace RobotWare.SolidWorks
+{
+  public class BitmapHandler : IDisposable
+  {
+    private IList<string> mFilePaths = new List<string>();
+
+    public string CreateFileFromResourceBitmap(string bitmapName, Assembly assy)
+    {
+      Stream manifestResStrm = null;
+      Bitmap bmp = null;
+      try
+      {
+        manifestResStrm = assy.GetManifestResourceStream(bitmapName);
+        bmp = new Bitmap(manifestResStrm);
+        var tempFileName = Path.GetTempFileName();
+        bmp.Save(tempFileName);
+        mFilePaths.Add(tempFileName);
+
+        return tempFileName;
+      }
+      catch (Exception)
+      {
+        return string.Empty;
+      }
+      finally
+      {
+        bmp.Dispose();
+        manifestResStrm.Close();
+        manifestResStrm = null;
+      }
+    }
+
+    public void Dispose()
+    {
+      foreach (var thisBmp in mFilePaths)
+      {
+        try
+        {
+          File.Delete(thisBmp);
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine(ex.Message);
+        }
+      }
+    }
+  }
+}
